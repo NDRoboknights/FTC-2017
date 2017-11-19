1f51a860d3aa2c5a95d8ebce1ce20d3c7b94cc1a
SirRender00 <evyn.machi@gmail.com>
2017-11-18 21:11:36 -0800

    Updated

--- V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/teleop/full/WorkingTeleOp.java
+++ V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/teleop/full/WorkingTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.team4348.teleop.test;
+package org.firstinspires.ftc.team4348.teleop.full;
 
 import org.firstinspires.ftc.team4348.robots.WorkingBot;
 import org.firstinspires.ftc.team4348.teleop.CustomTeleOp;
