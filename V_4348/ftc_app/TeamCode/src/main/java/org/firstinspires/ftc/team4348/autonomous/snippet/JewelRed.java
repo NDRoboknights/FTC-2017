1c800491c11f27153d47e9b9e5b94e86278c1c19
TeckDeck <deckergame.danny@gmail.com>
2017-11-20 21:08:45 -0800

    .

--- V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/autonomous/snippet/JewelRed.java
+++ V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/autonomous/snippet/JewelRed.java
@@ -16,27 +16,27 @@ public class JewelRed
     {
         //drop jewel servo arm
         bot.jewelServo.setPosition(0.0);
-        delay(200);
 
         //read color sensor and choose direction
         Direction dir = null;
 
-        //RGBA.red returns [0,255]
+        while(dir == null) {
+            //RGBA.red returns [0,255]
 
-        //if above threshold, choose that direction
-        if (bot.cSensor1.red() >= CustomAutonomous.COLOR_THRESHOLD) {
-            dir = Direction.BACKWARD;
-        }
-        //not an else statement to make sure we have a reading
-        else if (bot.cSensor1.blue() >= CustomAutonomous.COLOR_THRESHOLD) {
-            dir = Direction.FORWARD;
-        }
+            //if above threshold, choose that direction
+            if (bot.cSensor1.red() >= CustomAutonomous.COLOR_THRESHOLD) {
+                dir = Direction.BACKWARD;
+            }
+            //not an else statement to make sure we have a reading
+            else if (bot.cSensor1.blue() >= CustomAutonomous.COLOR_THRESHOLD) {
+                dir = Direction.FORWARD;
+            }
 
-        if (dir != null) { //make sure we had a reading
-            bot.setDrivePowerT(dir.v * bot.AUTO_DRIVE_SPEED, dir.v * bot.AUTO_DRIVE_SPEED, 200);
-            delay(50);
+            if (dir != null) { //make sure we had a reading
+                bot.setDrivePowerT(dir.v * bot.AUTO_DRIVE_SPEED, dir.v * bot.AUTO_DRIVE_SPEED, 200);
+                delay(50);
+            }
         }
-
         //retract arm
         bot.jewelServo.setPosition(bot.JEWEL_INIT_POS);
     }
