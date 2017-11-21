bbc18c4b08ef141eecf92e6275c31ad716edb2d6
SirRender00 <evyn.machi@gmail.com>
2017-11-20 16:56:40 -0800

    Restructured PID classes

--- V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/sensortest/ColorTest.java
+++ V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/sensortest/ColorTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.team4348.sensortest;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.team4348.robots.ColorBot;
 import org.firstinspires.ftc.team4348.teleop.CustomTeleOp;
 
@@ -7,6 +9,7 @@ import org.firstinspires.ftc.team4348.teleop.CustomTeleOp;
  * Created by RoboKnights on 11/20/2017.
  */
 
+@TeleOp(name="SensorTest: Color",group="SensorTest")
 public class ColorTest extends CustomTeleOp
 {
     ColorBot bot = new ColorBot();
