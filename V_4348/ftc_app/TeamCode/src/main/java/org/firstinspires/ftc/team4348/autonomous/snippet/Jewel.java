a84e102d2063610819edf09ecf7705d99ce3d32e
SirRender00 <32499767+SirRender00@users.noreply.github.com>
2017-11-21 18:19:49 -0800

    Created IDVuMark

--- V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/autonomous/snippet/Jewel.java
+++ V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/autonomous/snippet/Jewel.java
@@ -1,19 +1,19 @@
 package org.firstinspires.ftc.team4348.autonomous.snippet;
 
 import org.firstinspires.ftc.team4348.autonomous.CustomAutonomous;
-import org.firstinspires.ftc.team4348.utils.Direction;
 import org.firstinspires.ftc.team4348.robots.WorkingBot;
+import org.firstinspires.ftc.team4348.utils.Direction;
 import org.firstinspires.ftc.team4348.utils.TimeChecker;
 
 import static org.firstinspires.ftc.team4348.utils.Utilities.delay;
 
 /**
- * Created by RoboKnights on 11/19/2017.
+ * Created by Evyn on 11/21/2017.
  */
 
-public class JewelBlue
+public class Jewel
 {
-    public static Direction run(WorkingBot bot, long timeout)
+    public static Direction blueRun(WorkingBot bot, long timeout)
     {
         //drop jewel servo arm
         bot.jewelServo.setPosition(0.0);
@@ -31,6 +31,38 @@ public class JewelBlue
             }
             //not an else statement to make sure we have a reading
             else if (bot.cSensor1.red() >= CustomAutonomous.COLOR_THRESHOLD) {
+                dir = Direction.FORWARD;
+            }
+        }
+
+        if(dir != null) {
+            bot.setDrivePowerT(dir.v * bot.AUTO_DRIVE_SPEED, dir.v * bot.AUTO_DRIVE_SPEED, 200);
+            delay(50);
+        }
+
+        //retract arm
+        bot.jewelServo.setPosition(bot.JEWEL_INIT_POS);
+        return dir;
+    }
+
+    public static Direction redRun(WorkingBot bot, long timeout)
+    {
+        //drop jewel servo arm
+        bot.jewelServo.setPosition(0.0);
+
+        //read color sensor and choose direction
+        Direction dir = null;
+
+        TimeChecker tChecker = new TimeChecker(timeout);
+        while(dir == null && tChecker.checkStatus())
+        { //make sure we had a reading
+            //RGBA.red returns [0,255]
+            //if above threshold, choose that direction
+            if (bot.cSensor1.red() >= CustomAutonomous.COLOR_THRESHOLD) {
+                dir = Direction.BACKWARD;
+            }
+            //not an else statement to make sure we have a reading
+            else if (bot.cSensor1.blue() >= CustomAutonomous.COLOR_THRESHOLD) {
                 dir = Direction.FORWARD;
             }
         }
