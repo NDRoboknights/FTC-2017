498efd96cf7182995248ade81a4a9f97dc8cb730
SirRender00 <evyn.machi@gmail.com>
2017-12-16 13:11:18 -0800

    .

--- V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/robots/ColorBot.java
+++ V_4348/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/team4348/robots/ColorBot.java
@@ -1,17 +1,20 @@
 package org.firstinspires.ftc.team4348.robots;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 
 
 public class ColorBot extends Bot
 {
-    public ColorSensor cSensor;
+    public ModernRoboticsI2cColorSensor cSensor;
 
     @Override
     public void init(HardwareMap hMap)
     {
         this.hardwareMap = hMap;
-        cSensor = hMap.colorSensor.get(HardwareName.COLOR_SENSOR1.name);
+        cSensor = hardwareMap.get(ModernRoboticsI2cColorSensor.class, HardwareName.COLOR_SENSOR1.name);
+        cSensor.setI2cAddress(I2cAddr.create7bit(0x4c));
     }
 }
